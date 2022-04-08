77bf64b5d54091708f985a7cf6323801c58d7643
“David” <arias.maulen@gmail.com>
2022-04-08 16:58:23 -0400

    Correccion de clg

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ let contadorPalabras = document.querySelector('p');
 let totalFrase;
 contenedor.addEventListener('input', function() {
     contadorLetras.innerHTML = `Letras: ${contenedor.value.length}`
-    let palabras = contenedor.value.split(" ")
-    totalFrase = palabras
+    let palabras = contenedor.value.split(" ");
+    totalFrase = palabras;
     if (!palabras.includes('')) {
         contadorPalabras.innerHTML = `Palabras: ${totalFrase.length}`
-        console.log(totalFrase);
-        console.log('Largo de contenedor vacío: ', contenedor.value.length);
     }
 });
