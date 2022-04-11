a910049cee411d9d27277e64c1bc520f0f78d421
“David” <arias.maulen@gmail.com>
2022-04-11 15:27:04 -0400

    Correcciones de contador de palabras

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 let contenedor = document.querySelector('textarea');
 let contadorLetras = document.querySelector('span');
 let contadorPalabras = document.querySelector('p');
-let totalFrase;
+let totalFrase = [];
+let contador;
+console.log(totalFrase);
 contenedor.addEventListener('input', function() {
-    contadorLetras.innerHTML = `Letras: ${contenedor.value.length}`
-    let palabras = contenedor.value.split(" ");
-    totalFrase = palabras;
-    if (!palabras.includes('')) {
-        contadorPalabras.innerHTML = `Palabras: ${totalFrase.length}`
+    contadorLetras.innerHTML = `Letras: ${contenedor.value.length}`;
+    totalFrase = contenedor.value.split(" ");
+    console.log(totalFrase);
+    for (i = 0; i < totalFrase.length; i++) {
+        console.log(totalFrase);
+        contador = totalFrase.filter(elemento => elemento !== '');
+        contadorPalabras.innerHTML = `Palabras: ${contador.length}`;
     }
 });
